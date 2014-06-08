953398fcf4edc0b5619e64f2f76fdcacef6398bf
Kevin Schröder <Kevin.Schroeder@uni-duesseldorf.de>
2014-06-08 13:19:43 +0200

    monster

--- DNmonsters/src/DNmonsters/src/Monster.java
+++ DNmonsters/src/DNmonsters/src/Monster.java
@@ -4,8 +4,25 @@ public class Monster {
 	public Stats stats;
 	public int[] skills;
 	public Klasse klasse[]=new Klasse[2];
+    public String name;
 
-    public Monster(int level,int lifemax, int life, int energymax, int physdmg, int physdeff, int eledmg, int eledeff,int speed, int dodge,int expnext,int precision){
-
+    public Monster(String name,int level,int lifemax, int life, int physdmg, int physdeff, int eledmg, int eledeff,int speed, int dodge,int expnext,int precision,String Klasse1,String Klasse2){
+        this.name=name;
+        stats.set_eledeff(eledeff);
+        stats.set_dodge(dodge);
+        stats.set_eledmg(eledmg);
+        stats.set_physdmg(physdmg);
+        stats.set_precision(precision);
+        stats.set_lifemax(lifemax);
+        stats.set_life(life);
+        stats.set_physdeff(physdeff);
+        stats.set_speed(speed);
+        klasse[1].set_Klasse(Klasse1);
+        klasse[2].set_Klasse(Klasse2);
+        int i=1;
+        while (level>i){
+            level_up();
+        }
     }
+    public void level_up(){}
 }
---
+++ DNmonsters/src/DNmonsters/src/Monsters/Yggdrasil.java
@@ -0,0 +1,12 @@
+package DNmonsters.src.Monsters;
+
+import DNmonsters.src.Monster;
+
+/**
+ * Created by kevinschroeder on 08.06.14.
+ */
+public class Yggdrasil extends Monster {
+    public Yggdrasil(int level){
+        super("Yggdrasil",level,150,150,20,30,22,28,20,0,100,5,"Flora","Lux");
+    }
+}
